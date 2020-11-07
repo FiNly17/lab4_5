f9fd4a94f49c18888f608e2c11b2ebe523ddca94
FiN <danik.fin07@gmail.com>
2020-11-07 12:15:35 +0300

    lab4

---
+++ lab4/List.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Versioning;
+
+namespace lab4
+{
+#pragma warning disable CS0660 // Тип определяет оператор == или оператор !=, но не переопределяет Object.Equals(object o)
+#pragma warning disable CS0661 // Тип определяет оператор == или оператор !=, но не переопределяет Object.GetHashCode()
+    class MyList
+#pragma warning restore CS0661 // Тип определяет оператор == или оператор !=, но не переопределяет Object.GetHashCode()
+#pragma warning restore CS0660 // Тип определяет оператор == или оператор !=, но не переопределяет Object.Equals(object o)
+    {
+        public List<string> Values { get; private set; }
+
+        public Owner Info { get; private set; }
+        public DateTime Date { get; private set; }
+        public int Count { get { return Values.Count; } }
+
+        //конструкторы
+
+        public MyList(string name, string surname, int id, params dynamic[] items)
+        {
+            Date = DateTime.Now;
+            Info = new Owner(name, surname, id);
+            Values = new List<string>();
+            foreach (var item in items)
+            {
+                Values.Add(item.ToString());
+            }
+        }
+
+        public MyList()
+        {
+            Date = DateTime.Now;
+            Info = new Owner("-", "-", 0);
+            Values = new List<string>();
+        }
+
+        public string this[int index]
+        {
+            get => Values[index];
+            set => Values[index] = value;
+        }
+
+        public void Add(dynamic item)
+        {
+            Values.Add(item.ToString());
+        }
+
+        public void AddFirst(dynamic item)
+        {
+            Values.Insert(0, item.ToString());
+        }
+
+        public void RemoveFirst()
+        {
+            Values.RemoveAt(0);
+        }
+
+       
+        public static MyList operator +(dynamic item, MyList list)
+        {
+            MyList result = new MyList(list.Info.Name, list.Info.Surname, list.Info.Id);
+            foreach (var elem in list.Values)
+            {
+                result.Add(elem);
+            }
+            result.AddFirst(item);
+            return result;
+        }
+
+        public static MyList operator --(MyList list)
+        {
+            MyList result = new MyList(list.Info.Name, list.Info.Surname, list.Info.Id);
+            foreach (var item in list.Values)
+            {
+                result.Add(item);
+            }
+            result.RemoveFirst();
+            return result;
+        }
+
+
+        public static bool operator !=(MyList list1, MyList list2)
+        {
+            if (list1.Count != list2.Count)
+                return true;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1.Values[i] != list2.Values[i])
+                    return true;
+            }
+            return false;
+        }
+        public static MyList operator >(MyList list1, MyList list2)
+        {
+            MyList result = new MyList(list2.Info.Name, list2.Info.Surname, list2.Info.Id);
+            foreach (var item in list1.Values)
+            {
+                list2.Add(item);
+            }
+            return list2;
+        }
+        public static MyList operator <(MyList list1,MyList list2)
+        {
+            MyList result = new MyList(list1.Info.Name, list1.Info.Surname, list1.Info.Id);
+            foreach (var item in list2.Values)
+            {
+                list1.Add(item);
+            }
+            return list1;  
+        }
+        public static MyList operator !(MyList list1)
+        {
+            MyList result = new MyList(list1.Info.Name, list1.Info.Surname, list1.Info.Id);
+            foreach(var item in list1.Values)
+            {
+                result.AddFirst(item);
+            }
+            return result;
+        }
+        public static bool operator ==(MyList list1, MyList list2)
+        {
+            if (list1.Count != list2.Count)
+                return false;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1.Values[i] != list2.Values[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public static MyList operator +(MyList list1, MyList list2)
+        {
+            MyList result = new MyList(list1.Info.Name, list1.Info.Surname, list1.Info.Id);
+            foreach (var item in list1.Values)
+            {
+                result.Add(item);
+            }
+            result.Values.AddRange(list2.Values);
+            return result;
+        }
+
+        public class Owner
+        {
+            public Owner(string name, string surname, int id)
+            {
+                Name = name;
+                Surname = surname;
+                Id = id;
+            }
+
+            public Owner(Owner father)
+            {
+                Name = father.Name;
+                Surname = father.Surname;
+                Id = father.Id;
+            }
+
+            public string Name { get; private set; }
+            public string Surname { get; private set; }
+            public int Id { get; private set; }
+        }
+    }
+
+    static class StatisticOperation
+    {
+        public static string Rem(this MyList list1,int chislo)
+        {
+            Console.WriteLine("Список: ");
+            foreach (var item in list1.Values)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Выберите какой элемент вы хотите сократить: ");
+            int v = Convert.ToInt32(Console.ReadLine());
+            string q=list1.Values[v].Remove(chislo);
+            return q;
+        }
+
+        public static int Count(MyList list)
+        {
+            return list.Count;
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            
+            while (true)
+            {
+                var list1 = new MyList("dannil", "finskiy", 1, "text", 111, "Text111", "TEXt111");
+                var list1copy = new MyList("dannil", "finskiy", 1, "text", 111, "Text111", "TEXt111");
+                var list2 = new MyList("vlad", "simakovich", 2, "text", 222, "Text222");
+                var list3 = new MyList("zayac", "roman", 3, "text", 333, "Text333", 333);
+                var list3copy = new MyList("zayac", "roman", 3, "text", 333, "Text333", 333);
+
+                //операторы 
+                var list4 = !list1;
+                var list5 = list1 + list2;
+                Console.WriteLine(
+                    "1 - усечение строки до заданной длины" +
+                    "\n2 - вывод объектов класса " +
+                    "\n3 - сумма элементов списка " +
+                    "\n4 - инверсия элементов списка " +
+                    "\n5 - объединение двух списков" +
+                    "\n6 - проверка на неравенство" +
+                    "\n7 - добавление одного списка к другому"
+                    );
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите длину: ");
+                            int ch = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(
+                                "1 - list1" +
+                                "\n2 - list2" +
+                                "\n3 - list3" +
+                                "\n4 - list4" +
+                                "\n5 - list5"
+                                );
+                            int chisl = Convert.ToInt32(Console.ReadLine());
+                            switch (chisl)
+                            {
+                                case 1:
+                                    Console.WriteLine("Сокращенный элемент: " + list1.Rem(ch));
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Сокращенный элемент: " + list2.Rem(ch));
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Сокращенный элемент: " + list3.Rem(ch));
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Сокращенный элемент: " + list4.Rem(ch));
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Сокращенный элемент: " + list5.Rem(ch));
+                                    break;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("list1:");
+                            foreach (var item in list1.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine("\nlist2:");
+                            foreach (var item in list2.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine("\nlist3:");
+                            foreach (var item in list3.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine("\nlist3copy:");
+                            foreach (var item in list3copy.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine(
+                                "1 - длина списка list1" +
+                                "\n2 - длина списка list2" +
+                                "\n3 - длина списка list3" +
+                                "\n4 - длина списка list4" +
+                                "\n5 - длина списка list5"
+                                );
+                            int chisl = Convert.ToInt32(Console.ReadLine());
+                            switch (chisl)
+                            {
+                                case 1: Console.WriteLine("Длина списка list1: "+list1.Count);
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Длина списка list2: " + list2.Count);
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Длина списка list3: " + list3.Count);
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Длина списка list4: " + list4.Count);
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Длина списка list5: " + list5.Count);
+                                    break;
+                            }
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine($"список list1:");
+                            foreach (var item in list1.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine();
+                            Console.WriteLine($"инверсия элементов списка list1:");
+                            foreach (var item in list4.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine($"Объединение двух списков:");
+                            foreach (var item in list5.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                    case 6:
+                        {
+                            Console.WriteLine("Проверка на неравенство(true - равно, false - неравно):");
+                            Console.WriteLine($"list1 == list2: {list1 == list2} ");
+                            Console.WriteLine($"list3 == list3copy: {list3 == list3copy} ");
+                        }
+                        break;
+                    case 7:
+                        {
+
+                            var list6 = list1 < list2;
+                            Console.WriteLine($"Присоединение list2 k list1: ");
+                            foreach (var item in list6.Values)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+    }
+}
